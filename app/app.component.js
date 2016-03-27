1dcba20219758e895c1f6cf0991be129c6aaab7e
James Traggianese <james@hubspot.com>
2016-03-27 11:05:39 -0400

    Working api

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/common', 'rxjs/add/operator/debounceTime', 'rxjs/add/operator/map'], function(exports_1) {
+System.register(['angular2/core', './post.service', "angular2/http"], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,134 +8,33 @@ System.register(['angular2/core', 'angular2/common', 'rxjs/add/operator/debounce
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, common_1;
+    var core_1, post_service_1, http_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (common_1_1) {
-                common_1 = common_1_1;
+            function (post_service_1_1) {
+                post_service_1 = post_service_1_1;
             },
-            function (_1) {},
-            function (_2) {}],
+            function (http_1_1) {
+                http_1 = http_1_1;
+            }],
         execute: function() {
             AppComponent = (function () {
-                function AppComponent(fb) {
-                    this.form = fb.group({
-                        search: []
-                    });
-                    var search = this.form.find('search');
-                    search.valueChanges
-                        .debounceTime(400)
-                        .map(function (str) { return str.replace(' ', '-'); })
-                        .subscribe(function (x) { return console.log(x); });
-                    // var startDates = [];
-                    // var startDate = new Date();
-                    //
-                    // for (var day = -2; day <= 2; day++){
-                    //     var date = new Date(
-                    //       startDate.getFullYear(),
-                    //       startDate.getMonth(),
-                    //       startDate.getDate() + day);
-                    //     startDates.push(date);
-                    //
-                    //     Observable
-                    //         .fromArray(startDates)
-                    //         // .map(date => console.log(`Getting deals for date  ${date}`))
-                    //         .map(date => {
-                    //
-                    //             console.log(`Getting deals for date ${date}`);
-                    //             return [1, 2, 3];
-                    //
-                    //         })
-                    //         .subscribe(x => console.log(x));
-                    // }
-                    // function obsTest(x: Observable) {
-                    //
-                    //     x
-                    //         .flatMap(x => {
-                    //             console.log('calling the server for the latest news')
-                    //             return Observable.of([1, 2, 3]);
-                    //         })
-                    //         .subscribe(x => console.log(x));
-                    // }
-                    // obsTest(Observable.of(12));
-                    // obsTest(Observable.empty());
-                    // obsTest(Observable.range(1, 80));
-                    // obsTest(Observable.interval(1000));
-                    // var userStream = Observable.of({
-                    //     userId: 1, username: 'mosh'
-                    // }).delay(2000);
-                    //
-                    // var tweetStream = Observable.of([1, 2, 3]).delay(1500);
-                    //
-                    // Observable
-                    //     .forkJoin(userStream, tweetStream)
-                    //     .map(joined =>
-                    //         new Object({user: joined[0], tweets: joined[1] }))
-                    //     .subscribe(result => console.log(result));
-                    //handling errors
-                    // var observable = Observable.throw(new Error('something failed.'));
-                    //
-                    // observable.subscribe(
-                    //     x => console.log(x),
-                    //     error => console.error(error)
-                    // );
-                    //retrying
-                    // var counter = 0;
-                    //
-                    // var ajaxCall = Observable.of('url')
-                    //     .flatMap(() => {
-                    //         if (++counter < 2)
-                    //             return Observable.throw(new Error('request failed'));
-                    //
-                    //         return Observable.of([1, 2, 3]);
-                    //     });
-                    // ajaxCall
-                    //     .retry(3)
-                    //     .subscribe(
-                    //         x => console.log(x),
-                    //         error => console.log(error)
-                    //     );
-                    // var remoteDataStream = Observable.of([4, 5, 6]);
-                    //
-                    // remoteDataStream
-                    //     .catch(err => {
-                    //         var localDataStream = Observable.of([1, 22, 333]);
-                    //         return localDataStream;
-                    //     })
-                    //     .subscribe(x => console.log(x));
-                    //
-                    //
-                    // var remoteDataStream = Observable.of([1, 2, 3]).delay(5000);
-                    //
-                    // remoteDataStream
-                    //     .timeout(1000)
-                    //     .subscribe(
-                    //         x => console.log(x),
-                    //         error => console.log(error),
-                    //         () => console.log("completed")
-                    //     );
-                    //    
-                    // var observable = Observable.fromArray([1, 2, 3]);
-                    //
-                    // observable
-                    //     .flatMap(x => Observable.of(['a', 'b', 'c']))
-                    //     .subscribe(x => console.log(x));
-                    //
-                    // Observable.forkJoin(
-                    //     Observable.of({ username: 'mosh'}),
-                    //     Observable.of([1, 2, 3])
-                    // ).subscribe(x => console.log(x[0]));
+                function AppComponent(_postService) {
+                    this._postService = _postService;
+                    this._postService.getPosts()
+                        .subscribe(function (posts) { return console.log(posts); });
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n      <form [ngFormModel]=\"form\">\n        <input type=\"text\" ngControl=\"search\">\n      </form>\n  "
+                        template: "\n      <h1>This is my first angular app</h1>\n  ",
+                        providers: [post_service_1.PostService, http_1.HTTP_PROVIDERS]
                     }), 
-                    __metadata('design:paramtypes', [common_1.FormBuilder])
+                    __metadata('design:paramtypes', [post_service_1.PostService])
                 ], AppComponent);
                 return AppComponent;
             })();
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,144 +1,18 @@
 import {Component} from 'angular2/core';
-import {ControlGroup, FormBuilder} from 'angular2/common';
-
-import {Observable} from "rxjs/Rx";
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/map';
-import {error} from "util";
+import {PostService} from './post.service';
+import {HTTP_PROVIDERS} from "angular2/http";
 
 @Component({
     selector: 'my-app',
     template: `
-      <form [ngFormModel]="form">
-        <input type="text" ngControl="search">
-      </form>
-  `
+      <h1>This is my first angular app</h1>
+  `,
+    providers: [PostService, HTTP_PROVIDERS]
 })
 export class AppComponent {
-    form: ControlGroup;
-    constructor(fb: FormBuilder){
-        this.form = fb.group({
-            search: []
-        });
-
-        var search = this.form.find('search');
-        search.valueChanges
-            .debounceTime(400)
-            .map(str => (<string>str).replace(' ', '-'))
-            .subscribe(x => console.log(x));
-
-        // var startDates = [];
-        // var startDate = new Date();
-        //
-        // for (var day = -2; day <= 2; day++){
-        //     var date = new Date(
-        //       startDate.getFullYear(),
-        //       startDate.getMonth(),
-        //       startDate.getDate() + day);
-        //     startDates.push(date);
-        //
-        //     Observable
-        //         .fromArray(startDates)
-        //         // .map(date => console.log(`Getting deals for date  ${date}`))
-        //         .map(date => {
-        //
-        //             console.log(`Getting deals for date ${date}`);
-        //             return [1, 2, 3];
-        //
-        //         })
-        //         .subscribe(x => console.log(x));
-        // }
-
-
-        // function obsTest(x: Observable) {
-        //
-        //     x
-        //         .flatMap(x => {
-        //             console.log('calling the server for the latest news')
-        //             return Observable.of([1, 2, 3]);
-        //         })
-        //         .subscribe(x => console.log(x));
-        // }
-        // obsTest(Observable.of(12));
-        // obsTest(Observable.empty());
-        // obsTest(Observable.range(1, 80));
-        // obsTest(Observable.interval(1000));
-
-
-
-        // var userStream = Observable.of({
-        //     userId: 1, username: 'mosh'
-        // }).delay(2000);
-        //
-        // var tweetStream = Observable.of([1, 2, 3]).delay(1500);
-        //
-        // Observable
-        //     .forkJoin(userStream, tweetStream)
-        //     .map(joined =>
-        //         new Object({user: joined[0], tweets: joined[1] }))
-        //     .subscribe(result => console.log(result));
-
-
-        //handling errors
-
-        // var observable = Observable.throw(new Error('something failed.'));
-        //
-        // observable.subscribe(
-        //     x => console.log(x),
-        //     error => console.error(error)
-        // );
-
-
-        //retrying
-
-        // var counter = 0;
-        //
-        // var ajaxCall = Observable.of('url')
-        //     .flatMap(() => {
-        //         if (++counter < 2)
-        //             return Observable.throw(new Error('request failed'));
-        //
-        //         return Observable.of([1, 2, 3]);
-        //     });
-        // ajaxCall
-        //     .retry(3)
-        //     .subscribe(
-        //         x => console.log(x),
-        //         error => console.log(error)
-        //     );
-
-
-
-        // var remoteDataStream = Observable.of([4, 5, 6]);
-        //
-        // remoteDataStream
-        //     .catch(err => {
-        //         var localDataStream = Observable.of([1, 22, 333]);
-        //         return localDataStream;
-        //     })
-        //     .subscribe(x => console.log(x));
-        //
-        //
-        // var remoteDataStream = Observable.of([1, 2, 3]).delay(5000);
-        //
-        // remoteDataStream
-        //     .timeout(1000)
-        //     .subscribe(
-        //         x => console.log(x),
-        //         error => console.log(error),
-        //         () => console.log("completed")
-        //     );
-
-        //    
-        // var observable = Observable.fromArray([1, 2, 3]);
-        //
-        // observable
-        //     .flatMap(x => Observable.of(['a', 'b', 'c']))
-        //     .subscribe(x => console.log(x));
-        //
-        // Observable.forkJoin(
-        //     Observable.of({ username: 'mosh'}),
-        //     Observable.of([1, 2, 3])
-        // ).subscribe(x => console.log(x[0]));
+    constructor(private _postService: PostService){
+        this._postService.getPosts()
+            .subscribe(posts => console.log(posts));
     }
+    
 }
