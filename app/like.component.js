ffa14cda7606677c1807ab20c6d5dca0aaeb91fd
James Traggianese <james@hubspot.com>
2016-03-27 13:03:53 -0400

    Compile

--- app/like.component.js
+++ app/like.component.js
@@ -1,6 +1,4 @@
-System.register(['angular2/core', "./like.service"], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
+System.register(['angular2/core', "./like.service"], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -44,7 +42,7 @@ System.register(['angular2/core', "./like.service"], function(exports_1, context
                     __metadata('design:paramtypes', [like_service_1.LikeService])
                 ], LikeComponent);
                 return LikeComponent;
-            }());
+            })();
             exports_1("LikeComponent", LikeComponent);
         }
     }
--- app/like.service.js
+++ app/like.service.js
@@ -1,6 +1,4 @@
-System.register([], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
+System.register([], function(exports_1) {
     var LikeService;
     return {
         setters:[],
@@ -12,7 +10,7 @@ System.register([], function(exports_1, context_1) {
                     return 10;
                 };
                 return LikeService;
-            }());
+            })();
             exports_1("LikeService", LikeService);
         }
     }
